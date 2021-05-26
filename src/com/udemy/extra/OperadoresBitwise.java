c8234432609b69460a58b7192488f9c16f23be2f
Wendell Alves <wendellmccaulay@hotmail.com>
2021-05-25 22:54:16 -0300

    Operadores bit a bit

---
+++ src/com/udemy/extra/OperadoresBitwise.java
@@ -0,0 +1,15 @@
+package com.udemy.extra;
+
+public class OperadoresBitwise {
+
+    public static void main(String[] args) {
+
+        int n1 = 89;
+        int n2 = 60;
+        System.out.println(n1 & n2); // OPERAÇÃO E BIT A BIT
+        System.out.println(n1 | n2); // OPERAÇÃO OU BIT A BIT
+        System.out.println(n1 ^ n2); // OPERAÇÃO OU EXCLUSIVO BIT A BIT
+
+    }
+
+}
