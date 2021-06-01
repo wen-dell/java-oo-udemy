d9dbe0288ef84118ce437bb1010513b5319c61a6
Wendell Alves <wendellmccaulay@hotmail.com>
2021-05-31 22:05:31 -0300

    Criando uma classe para representar um triângulo

--- src/com/udemy/oo/application/ProgramWithoutOO.java
+++ src/com/udemy/oo/application/ProgramWithoutOO.java
@@ -1,36 +1,31 @@
 package com.udemy.oo.application;
 
-import com.udemy.oo.entities.Triangle;
-
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class ProgramWithoutOO {
 
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
-        Triangle x, y;
-        x = new Triangle();
-        y = new Triangle();
+        double xA, xB, xC, yA, yB, yC;
 
         System.out.println("Enter the measures of triangle X: ");
-        x.a = sc.nextDouble();
-        x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
 
         System.out.println("Enter the measures of triangle Y: ");
-        y.a = sc.nextDouble();
-        y.b = sc.nextDouble();
-        y.c = sc.nextDouble();
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
---
+++ src/com/udemy/oo/entities/Triangle.java
@@ -0,0 +1,9 @@
+package com.udemy.oo.entities;
+
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+}
