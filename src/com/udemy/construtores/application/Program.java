970e6ac5ac42a312aac7989761bc42d6cf2ad0ad
Wendell Alves <wendellmccaulay@hotmail.com>
2021-06-02 22:25:57 -0300

    Sobrecarga de construtores

--- src/com/udemy/construtores/application/Program.java
+++ src/com/udemy/construtores/application/Program.java
@@ -17,17 +17,15 @@ public class Program {
         String name = sc.nextLine();
         System.out.print("Price: ");
         double price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.println();
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added to stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
