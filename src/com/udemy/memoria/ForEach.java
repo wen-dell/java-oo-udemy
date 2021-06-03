8914e82deefbc733f995e5d656f40f2b3280f731
Wendell Alves <wendellmccaulay@hotmail.com>
2021-06-03 18:23:53 -0300

    Foreach com java

--- src/com/udemy/memoria/ForEach.java
+++ src/com/udemy/memoria/ForEach.java
@@ -1,4 +1,16 @@
 package com.udemy.memoria;
 
 public class ForEach {
+
+    public static void main(String[] args) {
+
+        String[] vect = new String[] { "Maria", "Bob", "Alex" };
+
+        for (String obj:
+             vect) {
+            System.out.println(obj);
+        }
+
+    }
+
 }
