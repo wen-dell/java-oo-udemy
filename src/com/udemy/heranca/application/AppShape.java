99dc0cee6e824e983d06015b152b3add9c770b93
Wendell Alves <wendellmccaulay@hotmail.com>
2021-06-27 13:21:02 -0300

    Exercício resolvido de métodos abstratos

---
+++ src/com/udemy/heranca/application/AppShape.java
@@ -0,0 +1,57 @@
+package com.udemy.heranca.application;
+
+
+import com.udemy.heranca.entities.Circle;
+import com.udemy.heranca.entities.Color;
+import com.udemy.heranca.entities.Rectangle;
+import com.udemy.heranca.entities.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AppShape {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Shape> shapes = new ArrayList<>();
+
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Shape #" + i + " data");
+            System.out.print("Rectangle or Cicle (r/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+
+            if (ch == 'r') {
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+                shapes.add(new Rectangle(color, width, height));
+            } else {
+                System.out.print("Radius: ");
+                double radius = sc.nextDouble();
+                shapes.add(new Circle(color, radius));
+            }
+        }
+
+        System.out.println();
+        System.out.println("SHAPE AREAS:");
+
+        for (Shape shape : shapes) {
+            System.out.println(String.format("%.2f", shape.area()));
+        }
+
+        sc.close();
+
+    }
+
+}
---
+++ src/com/udemy/heranca/entities/Color.java
@@ -0,0 +1,9 @@
+package com.udemy.heranca.entities;
+
+public enum Color {
+
+    BLACK,
+    BLUE,
+    RED;
+
+}
