ebd268f4a69253c53d2a44603cc06436062240b2
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-01 14:19:45 -0300

    Exibindo as informações do arquivo

---
+++ src/com/udemy/arquivos/FileInfo.java
@@ -0,0 +1,25 @@
+package com.udemy.arquivos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileInfo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+
+        sc.close();
+
+    }
+
+}
