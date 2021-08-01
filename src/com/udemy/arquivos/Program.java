31bc2e65394bfe8936be33ac5cc19c36ea26caee
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-01 12:46:05 -0300

    Ler arquivo com Scanner e File

---
+++ src/com/udemy/arquivos/Program.java
@@ -0,0 +1,28 @@
+package com.udemy.arquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        File file = new File("/home/wendell/arquivo.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+    }
+
+}
