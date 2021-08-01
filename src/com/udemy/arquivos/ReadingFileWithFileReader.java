00e648959ee76272ecc5475a70aa2746a9d2f6af
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-01 13:44:15 -0300

    Usando BufferedReader com try-with-resources

--- src/com/udemy/arquivos/ReadingFileWithFileReader.java
+++ src/com/udemy/arquivos/ReadingFileWithFileReader.java
@@ -9,32 +9,15 @@ public class ReadingFileWithFileReader {
     public static void main(String[] args) {
 
         String path = "";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
-
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
