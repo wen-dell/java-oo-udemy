00abe03711e5ac497cfc6afdeb1e7a51b04de157
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-02 22:56:50 -0300

    Criando classes de serviço e modelo

---
+++ src/com/udemy/interfaces/application/Program.java
@@ -0,0 +1,11 @@
+package com.udemy.interfaces.application;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/com/udemy/interfaces/model/services/RentalService.java
@@ -0,0 +1,24 @@
+package com.udemy.interfaces.model.services;
+
+import com.udemy.interfaces.model.entities.CarRental;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService() {
+    }
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+    }
+}
