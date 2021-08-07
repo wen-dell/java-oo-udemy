8cfcb84ca09e489915bac658068fd29a690ee435
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-07 10:56:53 -0300

    Solução do problema (com interface)

--- src/com/udemy/interfaces/model/services/BrazilTaxService.java
+++ src/com/udemy/interfaces/model/services/BrazilTaxService.java
@@ -1,6 +1,6 @@
 package com.udemy.interfaces.model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 
     public double tax(double amount) {
         if (amount <= 100.0) {
