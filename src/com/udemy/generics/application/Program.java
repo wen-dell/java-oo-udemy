53e8fce2a1d819d7a2e422144e0e6327bd15dc55
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-08 18:00:16 -0300

    Introdução ao Generics

---
+++ src/com/udemy/generics/application/Program.java
@@ -0,0 +1,30 @@
+package com.udemy.generics.application;
+
+import com.udemy.generics.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService ps = new PrintService();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+
+    }
+
+}
