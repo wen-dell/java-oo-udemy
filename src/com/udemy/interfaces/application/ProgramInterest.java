f511d3845179bdc3bad63d7ff804f753b49a952f
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-08 16:58:10 -0300

    Taxa de juros de 1% para service dos EUA

--- src/com/udemy/interfaces/application/ProgramInterest.java
+++ src/com/udemy/interfaces/application/ProgramInterest.java
@@ -1,6 +1,7 @@
 package com.udemy.interfaces.application;
 
 import com.udemy.interfaces.model.services.BrazilInterestService;
+import com.udemy.interfaces.model.services.UsaInterestService;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ public class ProgramInterest {
         System.out.print("Months: ");
         int months = sc.nextInt();
 
-        BrazilInterestService is = new BrazilInterestService(2.0);
+        UsaInterestService is = new UsaInterestService(1.0);
         double payment = is.payment(amount, months);
 
         System.out.println("Payment after " + months + " months:");
