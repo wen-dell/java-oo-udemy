d08d80151c71284024b57af2a471423ed055e4cd
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-09 19:44:08 -0300

    Trabalhando com curingas delimitados

--- src/com/udemy/generics/application/ProgramShape.java
+++ src/com/udemy/generics/application/ProgramShape.java
@@ -15,11 +15,15 @@ public class ProgramShape {
         myShapes.add(new Rectangle(3.0, 2.0));
         myShapes.add(new Circle(2.0));
 
-        System.out.printf("Total area: %.2f", totalArea(myShapes));
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
+        System.out.printf("Total area: %.2f", totalArea(myCircles));
 
     }
 
-    public static double totalArea(List<Shape> list) {
+    public static double totalArea(List<? extends Shape> list) {
         double sum = 0.0;
         for (Shape s : list) {
             sum += s.area();
