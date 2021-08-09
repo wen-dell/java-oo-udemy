bafacc37c387ecead6d9bf04c5c4560bc97f3c90
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-08 21:16:17 -0300

    Usando genérico delimitado

--- src/com/udemy/generics/application/Application.java
+++ src/com/udemy/generics/application/Application.java
@@ -1,5 +1,6 @@
 package com.udemy.generics.application;
 
+import com.udemy.generics.entities.Product;
 import com.udemy.generics.services.CalculationService;
 
 import java.io.BufferedReader;
@@ -7,22 +8,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-        String path = "/home/wendell/in.txt";
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+        String path = "/home/wendell/produtos.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
-                list.add(Integer.parseInt(line));
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
-            Integer x = CalculationService.max(list);
-            System.out.println("Max:");
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive:");
             System.out.println(x);
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
