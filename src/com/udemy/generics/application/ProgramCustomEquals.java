5f60e7d6e1b62f49ec5495c4a5604eff55b844e0
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-09 21:02:44 -0300

    Comparando referências

--- src/com/udemy/generics/application/ProgramCustomEquals.java
+++ src/com/udemy/generics/application/ProgramCustomEquals.java
@@ -12,6 +12,7 @@ public class ProgramCustomEquals {
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
 
     }
 
