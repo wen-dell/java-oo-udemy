e768e8542f4b5e89d776e3f6c75716cb5ec5e234
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-09 21:06:18 -0300

    Caso especial de comparação

--- src/com/udemy/generics/application/ProgramCustomEquals.java
+++ src/com/udemy/generics/application/ProgramCustomEquals.java
@@ -9,10 +9,17 @@ public class ProgramCustomEquals {
         Client c1 = new Client("Maria", "maria@gmail.com");
         Client c2 = new Client("Maria", "alexmaria@gmail.com");
 
+        String s1 = "Test";
+        String s2 = "Test";
+
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         System.out.println(c1.equals(c2));
         System.out.println(c1 == c2);
+
+        // O compilador dá um tratamento especial para literais, se você usar o new String()
+        // estará forçando uma referência, e o compilador não vai dar true
+        System.out.println(s1 == s2);
 
     }
 
