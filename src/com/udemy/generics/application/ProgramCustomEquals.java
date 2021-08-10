8037e389e1f85b9de8a7316892aebc33047eba9b
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-09 21:01:32 -0300

    Gerando hashcode e equals

---
+++ src/com/udemy/generics/application/ProgramCustomEquals.java
@@ -0,0 +1,18 @@
+package com.udemy.generics.application;
+
+import com.udemy.generics.entities.Client;
+
+public class ProgramCustomEquals {
+
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Maria", "alexmaria@gmail.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+
+    }
+
+}
---
+++ src/com/udemy/generics/entities/Client.java
@@ -0,0 +1,46 @@
+package com.udemy.generics.entities;
+
+public class Client {
+
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (!name.equals(client.name)) return false;
+        return email.equals(client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+}
