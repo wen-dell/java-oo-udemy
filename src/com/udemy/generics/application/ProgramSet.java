c045b5a514415d12d6fd48736cec05b80c1df233
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-11 19:22:01 -0300

    Trabalhando com tipo Set

---
+++ src/com/udemy/generics/application/ProgramSet.java
@@ -0,0 +1,28 @@
+package com.udemy.generics.application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProgramSet {
+
+    public static void main(String[] args) {
+
+        // addAll() -> união
+        // retainAll() -> interseção
+        // removeAll() -> diferença
+
+        Set<String> set = new HashSet<>();
+
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+
+    }
+
+}
