68b31b49db86d277719f0421a35abf5fc420f680
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-11 19:31:56 -0300

    Operações com união, interseção e diferença

--- src/com/udemy/generics/application/ProgramSet.java
+++ src/com/udemy/generics/application/ProgramSet.java
@@ -1,7 +1,9 @@
 package com.udemy.generics.application;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ProgramSet {
 
@@ -11,16 +13,39 @@ public class ProgramSet {
         // retainAll() -> interseção
         // removeAll() -> diferença
 
-        // Sem ordem, porém mais rápido
-        Set<String> set = new HashSet<>();
+        // Sem ordem, porém mais rápido (tabela de hash)
+        Set<String> hashSet = new HashSet<>();
+        // Com ordem, porém lento (árvore rubro negra)
+        Set<String> treeSet = new TreeSet<>();
+        // Mantém a ordem em que os elementos são adicionados
+        Set<String> linkedHashSet = new LinkedHashSet<>();
 
-        set.add("TV");
-        set.add("Tablet");
-        set.add("Notebook");
+        hashSet.add("TV");
+        hashSet.add("Tablet");
+        hashSet.add("Notebook");
 
-        System.out.println(set.contains("Notebook"));
+        treeSet.add("TV");
+        treeSet.add("Tablet");
+        treeSet.add("Notebook");
 
-        for (String p : set) {
+        linkedHashSet.add("TV");
+        linkedHashSet.add("Tablet");
+        linkedHashSet.add("Notebook");
+
+        hashSet.remove("Tablet");
+        hashSet.removeIf(x -> x.length() >= 3);
+
+        System.out.println(hashSet.contains("Notebook"));
+
+        for (String p : hashSet) {
+            System.out.println(p);
+        }
+
+        for (String p : treeSet) {
+            System.out.println(p);
+        }
+
+        for (String p : linkedHashSet) {
             System.out.println(p);
         }
 
