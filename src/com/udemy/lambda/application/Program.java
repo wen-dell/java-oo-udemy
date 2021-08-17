c293481be9eb1497b102c2166cc90cf06bec0997
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-16 23:05:55 -0300

    Classe anônima com Comparator

---
+++ src/com/udemy/lambda/application/Program.java
@@ -0,0 +1,35 @@
+package com.udemy.lambda.application;
+
+import com.udemy.lambda.entities.Product;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(new MyComparator());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+
+}
