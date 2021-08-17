1b5ac33c4312f42e4774bf2dfc219afc3a66d985
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-16 23:36:09 -0300

    Comparator em uma única linha

--- src/com/udemy/lambda/application/Program.java
+++ src/com/udemy/lambda/application/Program.java
@@ -17,11 +17,7 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = (p1, p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
-
-        list.sort(comp);
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
