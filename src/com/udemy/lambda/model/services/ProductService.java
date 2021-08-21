1d48b92fbb28019508ad12063c9881b1c9f33be7
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 17:53:33 -0300

    Passando uma função para dentro de outra

---
+++ src/com/udemy/lambda/model/services/ProductService.java
@@ -0,0 +1,20 @@
+package com.udemy.lambda.model.services;
+
+import com.udemy.lambda.entities.Product;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+
+    public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+        double sum = 0.0;
+        for (Product p : list) {
+            if (criteria.test(p)) {
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+
+}
