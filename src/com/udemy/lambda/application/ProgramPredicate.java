7cb12b6c3aacd1bd006bc175e277fa4ec3ecb2b9
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 16:46:20 -0300

    Passando um Predicate

--- src/com/udemy/lambda/application/ProgramPredicate.java
+++ src/com/udemy/lambda/application/ProgramPredicate.java
@@ -5,6 +5,7 @@ import com.udemy.lambda.util.ProductPredicate;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class ProgramPredicate {
 
@@ -17,7 +18,9 @@ public class ProgramPredicate {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = product -> product.getPrice() >= 100.00;
+
+        list.removeIf(pred);
 
         for (Product p : list) {
             System.out.println(p);
