f57df4a90f107e954eac74b88c637736652bc8ea
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 17:35:48 -0300

    Method reference não-estático

--- src/com/udemy/lambda/entities/Product.java
+++ src/com/udemy/lambda/entities/Product.java
@@ -46,6 +46,10 @@ public class Product {
         return product.getName().toUpperCase();
     }
 
+    public String nonStaticUpperCaseName() {
+        return getName().toUpperCase();
+    }
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
