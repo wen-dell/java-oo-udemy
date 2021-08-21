fde58b22b7fe3f1b5a1e4b87c0f49c601f07bd03
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 17:37:06 -0300

    Usando expressão lambda declarada

--- src/com/udemy/lambda/application/ProgramFunction.java
+++ src/com/udemy/lambda/application/ProgramFunction.java
@@ -5,6 +5,7 @@ import com.udemy.lambda.util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ProgramFunction {
@@ -18,7 +19,9 @@ public class ProgramFunction {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> function = product -> product.getName().toUpperCase();
+
+        List<String> names = list.stream().map(function).collect(Collectors.toList());
 
         names.forEach(System.out::println);
 
