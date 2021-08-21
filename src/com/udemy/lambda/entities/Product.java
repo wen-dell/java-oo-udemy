b90209703bcecd6e4fd469f598d42b74de9e10a3
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 17:34:11 -0300

    Method reference estático

--- src/com/udemy/lambda/entities/Product.java
+++ src/com/udemy/lambda/entities/Product.java
@@ -42,6 +42,10 @@ public class Product {
         setPrice(getPrice() * 1.1);
     }
 
+    public static String staticUpperCaseName(Product product) {
+        return product.getName().toUpperCase();
+    }
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
