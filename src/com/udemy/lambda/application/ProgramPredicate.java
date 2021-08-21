168d5c9e23df7d73726461e1dbae23aae73c10ba
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 15:58:52 -0300

    Predicados

---
+++ src/com/udemy/lambda/application/ProgramPredicate.java
@@ -0,0 +1,27 @@
+package com.udemy.lambda.application;
+
+import com.udemy.lambda.entities.Product;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProgramPredicate {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(p -> p.getPrice() >= 100.00);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+
+}
