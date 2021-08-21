4a8b9c93253ba12ba75a636d1ee31231309cf6a4
Wendell Alves <wendellmccaulay@hotmail.com>
2021-08-21 16:48:17 -0300

    Expressão lambda

--- src/com/udemy/lambda/application/ProgramPredicate.java
+++ src/com/udemy/lambda/application/ProgramPredicate.java
@@ -18,9 +18,7 @@ public class ProgramPredicate {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        Predicate<Product> pred = product -> product.getPrice() >= 100.00;
-
-        list.removeIf(pred);
+        list.removeIf(product -> product.getPrice() >= 100.00);
 
         for (Product p : list) {
             System.out.println(p);
